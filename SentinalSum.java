d4af90ea93b2a35113a94aebe00892349a7b9fe7
callumdbak <callumdbak@gmail.com>
2025-11-19 09:18:49 -0800

    Update SentinalSum.java

--- SentinalSum.java
+++ SentinalSum.java
@@ -1,3 +1,37 @@
-public class SentinalSum
-{
+import java.util.Scanner;
+
+public class SentinalSum {
+
+    public static void main(String[] args) {
+        calculateSum();
+    }
+
+    public static void calculateSum() {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int number;
+
+        System.out.print("Enter number: ");
+
+        if (scanner.hasNextInt()) {
+            number = scanner.nextInt();
+        } else {
+            System.out.print("Invalid input Sum is 0");
+            return;
+        }
+
+     while (number != -1) {
+         sum += number;
+            
+         System.out.print("Enter number: ");
+         if (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+         } else {
+             System.out.println("error");
+                number = -1; 
+     }
+     }
+
+        System.out.println("the sum is " + sum);
+    }
 }
